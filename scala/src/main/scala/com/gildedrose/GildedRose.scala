f49fbd9936eed9f72fdcc40bfd9548fce2011ac2
gamezftw <gamezftw@yahoo.coim>
2026-01-09 09:45:54 +0100

    refactor: extract update logic to item companion

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -3,10 +3,6 @@ package com.gildedrose
 class GildedRose(val items: Array[Item]) {
 
   def updateQuality(): Unit = {
-    items.foreach(i => {
-      val domainItem = ItemUpdater.updateItem(i)
-      i.sellIn = domainItem.sellIn
-      i.quality = domainItem.quality
-    })
+    items.foreach(i => i.fromItem(ItemUpdater.updateItem(i)))
   }
 }
--- scala/src/main/scala/com/gildedrose/Item.scala
+++ scala/src/main/scala/com/gildedrose/Item.scala
@@ -1,3 +1,10 @@
 package com.gildedrose
 
 case class Item(name: String, var sellIn: Int, var quality: Int) {}
+
+object Item {
+  extension (item: Item)
+    def fromItem(that: Item) =
+      item.sellIn = that.sellIn
+      item.quality = that.quality
+}
